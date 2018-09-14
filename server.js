bc3a80f53b1f9dfff00af971b1f78e30960799ca
Juan Betancourt <juan.c.betancourt@hotmail.com>
2018-09-13 22:40:15 -0700

    Updating CSS Pages

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.get('/', (req, res) => res.render('index'));
 app.get('/adminpage', adminAccess);
 
 //About us page
-app.get('/about', (req,res) => res.render('./pages/about/aboutus'))
+app.get('/about', (req,res) => res.render('./pages/about/creators'))
 
 // This path need a show.ejs to show only user's images.
 app.get('/client', (req,res) => res.render('./pages/clientAccess/clientView'));
